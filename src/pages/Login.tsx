2f105b7ed0577d77a486873168ff548fcbfd0956
amithasan102827 <amithasan102827@gmail.com>
2023-08-08 18:13:36 +0600

    user register and login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,6 @@ const Login = () => {
     const {
         register,
         handleSubmit,
-        // formState: { errors },
     } = useForm<LoginFormInputs>();
 
     const { user, isLoading } = useAppSelector((state) => state.user);
